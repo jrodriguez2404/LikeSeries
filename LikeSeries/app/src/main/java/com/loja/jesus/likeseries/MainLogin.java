1a485ece49ac6913ac2cadd273eace51d7929617
jrodriguez2404 <j.rodriguez@ponienteformacion.com>
2019-04-09 17:56:00 +0200

    Regitro acabado
    Parte del login/registro de la app finalizada

---
+++ LikeSeries/app/src/main/java/com/loja/jesus/likeseries/MainLogin.java
@@ -0,0 +1,188 @@
+package com.loja.jesus.likeseries;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainLogin extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    //Declaración de elementos del login
+    private Button isesion,iregistro;
+    //Login
+    private EditText temail, tcontra;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_login);
+        Toolbar appToolbar = (Toolbar) findViewById(R.id.toolbar);
+// *************************************************
+// Pongo el titulo en la toolbar
+        appToolbar.setTitle(getResources().getString(R.string.LOGIN));
+        isesion = findViewById(R.id.login);
+        temail = findViewById(R.id.temail);
+        tcontra = findViewById(R.id.tcontrasena);
+        iregistro=findViewById(R.id.registro);
+        iregistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MainRegistro.class);
+                startActivity(intent);
+            }
+        });
+        isesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.login:
+                        if (!temail.getText().toString().equals("")) {
+                            if (!tcontra.getText().toString().equals("")) {
+                                logearUsuarioFirebase(temail.getText().toString(), tcontra.getText().toString());
+                            } else {
+                                Toast.makeText(getApplicationContext(),
+                                        "Contraseña vacia", Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            Toast.makeText(getApplicationContext(),
+                                    "Email vacio", Toast.LENGTH_LONG).show();
+                            tcontra.setText("");
+                        }
+                        break;
+                }
+
+            }
+        });
+    }
+    //Arreglar , nunca se queda el usuario logeado
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            updateUI();
+        }
+    }
+/**
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        AlertDialog dialog = builder.create();
+        builder.setTitle("Salir");
+        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                cerrarPagina();
+                dialog.dismiss();
+            }
+        });
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+            }
+        });
+    }
+*/   public void cerrarPagina()
+    {
+        finish();
+    }
+    /**
+     * Método principal encargado de integrar al usuario en la aplicación si todos los valores son correctos y si el email de verificación esta true
+     * @param email
+     * @param password
+     */
+    private void logearUsuarioFirebase(final String email, final String password) {
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        String TAG = "";
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInWithEmail:success");
+                            user = mAuth.getInstance().getCurrentUser();
+                            if (user.isEmailVerified()) {
+                                updateUI();
+
+                            }
+                            else
+                            {
+                                Snackbar.make(getCurrentFocus(), "¿Email no verificado , reenviar email?", Snackbar.LENGTH_INDEFINITE)
+                                        .setActionTextColor(Color.CYAN)
+                                        .setActionTextColor(Color.GREEN)
+                                        .setAction("Aceptar", new View.OnClickListener() {
+                                            @Override
+                                           public void onClick(View view) {
+
+                                                user.sendEmailVerification();
+                                            }
+                                        })
+                                        .show();
+                                //FirebaseAuth.getInstance().signOut();
+                            }
+
+
+                        } else {
+                            // No se registra correctamente
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            try {
+                                throw task.getException();
+
+                            } catch (Exception e) {
+                                Snackbar.make(getCurrentFocus(), "Error al logear usuario", Snackbar.LENGTH_LONG)
+                                        .setActionTextColor(Color.CYAN)
+                                        .setActionTextColor(Color.GREEN)
+                                        .show();
+                            }
+                        }
+                    }
+                });
+    }
+    /**
+     * Metodo encargado de abrir la SplashScrren e insertar todos los atributos necesarios
+     * @param usuarionombre
+     * @param uid
+     * @param email
+     */
+    private void abrirSplashScreen(String usuarionombre,String uid,String email)
+    {
+        Intent intent = new Intent(getApplicationContext(),SplashScreen.class);
+        intent.putExtra("cambioclase", true);
+        intent.putExtra("nombre", usuarionombre);
+        intent.putExtra("uid", uid);
+        intent.putExtra("email",email);
+        startActivity(intent);
+    }
+    /**
+     * Método que se encarga de abrir la nueva pantalla de la actividad principal una vez logeado
+     */
+    private void updateUI()
+    {
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        String usuarionombre = user.getDisplayName();
+        String uid = user.getUid();
+        String email = user.getEmail();
+        abrirSplashScreen(usuarionombre,uid,email);
+        cerrarPagina();
+    }
+}
---
+++ LikeSeries/app/src/main/java/com/loja/jesus/likeseries/MainRegistro.java
@@ -0,0 +1,208 @@
+package com.loja.jesus.likeseries;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import static android.support.constraint.Constraints.TAG;
+
+public class MainRegistro extends AppCompatActivity {
+    //Registro
+    private Button registrar;
+    private EditText temailregistro, tcontrasenaregistro, tnombre;
+    //Mensajes y acuerdos
+    private CheckBox recibir, acuerdolegal;
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    private FirebaseFirestore db;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_registro);
+
+        Toolbar appToolbar = (Toolbar) findViewById(R.id.toolbar);
+// *************************************************
+// Pongo el titulo en la toolbar
+        appToolbar.setTitle(getResources().getString(R.string.REGISTRO));
+// Asigno la flecha de atras a la toolbar
+        appToolbar.setNavigationIcon(R.drawable.abc_ic_ab_back_material);
+// Hago que cuando se pulse la flecha de atras se cierre la actividad
+        appToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        tnombre = findViewById(R.id.nombre);
+        temailregistro = findViewById(R.id.temailregistro);
+        tcontrasenaregistro = findViewById(R.id.tcontrasenaregistro);
+
+
+        acuerdolegal = findViewById(R.id.acuerdolegal);
+        recibir = findViewById(R.id.mensajes);
+
+
+
+
+        registrar = findViewById(R.id.registrar);
+
+        registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Partimos de que el acuerdo legal esta checheado
+                if (!temailregistro.getText().toString().equals("")) {
+                    if (!tcontrasenaregistro.getText().toString().equals("")) {
+                        if (acuerdolegal.isChecked() == true) {
+                            //Registramos un usuario con firebase
+                            registrarUsuarioFirebase(temailregistro.getText().toString(), tcontrasenaregistro.getText().toString(), tnombre.getText().toString(), recibir.isChecked());
+
+
+                        }
+                        //Este else simplemente ejecuta un Toast que indica que el acuerdo legal es necesario , y pone el texto de color rojo
+                        else {
+
+                            tcontrasenaregistro.setText("");
+                            Toast.makeText(getApplicationContext(),
+                                    "Acuerdo Legal necesario", Toast.LENGTH_LONG).show();
+                            acuerdolegal.setTextColor(getResources().getColor(R.color.rojo));
+                        }
+                    } else {
+                        tcontrasenaregistro.setText("");
+                        Toast.makeText(getApplicationContext(),
+                                "Contraseña requerida", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    tcontrasenaregistro.setText("");
+                    Toast.makeText(getApplicationContext(),
+                            "Email requerido", Toast.LENGTH_LONG).show();
+                    temailregistro.setTextColor(getResources().getColor(R.color.rojo));
+                }
+            }
+        });
+    }
+
+    /**
+     * Registra el usuario
+     * @param email
+     * @param password
+     * @param nombre
+     * @param recibir
+     */
+    public void registrarUsuarioFirebase(String email , String password, final String nombre, final Boolean recibir) {
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            user = mAuth.getCurrentUser();
+                            Intent intent = new Intent(getApplication(), SplashScreen.class);
+                            intent.putExtra("cambioclase", false);
+                            startActivity(intent);
+                            enviarAuthEmail1(user.getUid(), nombre, recibir);
+                        }
+                        else
+                        {
+                            try {
+                                throw task.getException();
+                            } catch (FirebaseAuthWeakPasswordException e) {
+                                Toast.makeText(getApplicationContext(),
+                                        "Contraseña debil", Toast.LENGTH_LONG).show();
+                            } catch (FirebaseAuthInvalidCredentialsException e) {
+                                Toast.makeText(getApplicationContext(),
+                                        "Email invalido", Toast.LENGTH_LONG).show();
+
+                            } catch (FirebaseAuthUserCollisionException e) {
+                                Toast.makeText(getApplicationContext(),
+                                        "Usuario ya registrado", Toast.LENGTH_LONG).show();
+
+                            } catch (FirebaseAuthInvalidUserException e) {
+                                Toast.makeText(getApplicationContext(),
+                                        "Usuario invalido", Toast.LENGTH_LONG).show();
+
+                            } catch (Exception e) {
+                                Toast.makeText(getApplicationContext(),
+                                        "Error al registrar usuario", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+                });
+    }
+
+    /**
+     * Envia un correo de verificación , a parte inserta los datos en la BD , no obstante hasta que no nos registremos no podemos entrar
+     * @param uid
+     */
+    private void enviarAuthEmail1(final String uid, final String nombre, final Boolean recibir) {
+        user = mAuth.getInstance().getCurrentUser();
+        user.sendEmailVerification()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Email sent.");
+                            //Creo un usuario y lo agrego a la base de datos de FireBase Cloud
+
+
+                            Usuario usuario = new Usuario(nombre, user.getEmail(),uid ,recibir);
+
+                            insertarBasedeDatosFireBaseUsuario(usuario);
+
+
+                            //Cierro la actividad
+                            MainLogin login = new MainLogin();
+                            login.cerrarPagina();
+
+                        }
+                    }
+                });
+    }
+    /**
+     * Este metodo se encargara de guardar nuestros datos registrados en la nube
+     *
+     * @param usuario
+     */
+    private void insertarBasedeDatosFireBaseUsuario(Usuario usuario) {
+        // Creamos un usuario y lo guardamos en la base de datos
+        db = FirebaseFirestore.getInstance();
+        db.collection("usuarios").document(usuario.getToken())
+                .set(usuario, SetOptions.merge())
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+    }
+}
