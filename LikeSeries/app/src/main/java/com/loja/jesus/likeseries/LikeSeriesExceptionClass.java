c70c94eae5c70439e33098e3dd3d90219934454a
jrodriguez2404 <j.rodriguez@ponienteformacion.com>
2019-05-29 13:43:49 +0200

    Reboot

--- LikeSeries/app/src/main/java/com/loja/jesus/likeseries/LikeSeriesExceptionClass.java
+++
@@ -1,8 +0,0 @@
-package com.loja.jesus.likeseries;
-
-public class LikeSeriesExceptionClass extends Exception {
-
-    public LikeSeriesExceptionClass(String message) {
-        super(message);
-    }
-}
